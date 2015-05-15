0fe84a8b43480bc957f28031388e7c86fcb6354f
Patrick Barrett <patrickbarrett@exosite.com>
2015-05-15 13:02:32 -0500

    initial commit; holy shit that was easy

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+extern crate gtk;
+extern crate hyper;
+
+use gtk::traits::*;
+use gtk::signal::Inhibit;
+
+use std::io::Read;
+
+use hyper::Client;
+use hyper::header::Connection;
+
+fn main() {
+    gtk::init();
+
+    // Create Top Level Window
+    let window = gtk::Window::new(gtk::WindowType::TopLevel).unwrap();
+
+    // Setup Window Stuff
+    window.set_title("Exosite Timestamp Reader Thing");
+    window.set_border_width(10);
+    //window.set_window_position(gtk::WindowPosition::Center);
+    window.set_default_size(350, 70);
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(true)
+    });
+
+    // Create UI Elements
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 10).unwrap();
+    let label = gtk::Label::new("Click Update, I Dare You!").unwrap();
+    let button = gtk::Button::new_with_label("Update").unwrap();
+
+    // Connect Up Layout
+    vbox.add(&label);
+    vbox.add(&button);
+    window.add(&vbox);
+
+    // Setup Button Click Handler
+	button.connect_clicked(move |_| {
+        label.set_text(&get_time())
+    });
+
+	// Show and Run
+    window.show_all();
+    gtk::main();
+}
+
+
+
+fn get_time() -> String {
+    // Create a client.
+    let mut client = Client::new();
+
+    // Creating an outgoing request.
+    let mut res = client.get("http://m2.exosite.com/timestamp")
+        // set a header
+        .header(Connection::close())
+        // let 'er go!
+        .send().unwrap();
+
+    // Read the Response.
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+
+    body
+}
